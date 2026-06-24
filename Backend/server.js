09cac39ab8fe3ebaa5872998d23e37262ff9ae62
AqsaQayyum-333 <243547@students.au.edu.pk>
2026-06-24 21:16:23 +0500

    Backend Deployment Fixes

--- Backend/server.js
+++ Backend/server.js
@@ -37,6 +37,8 @@ app.get("/", (req, res) => {
 });
 
 // Start Server
-app.listen(5000, () => {
-    console.log("Server Running on Port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server Running on Port ${PORT}`);
 });
